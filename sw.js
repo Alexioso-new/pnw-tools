f5eab34b93af7dba278be12cd05f4716a695b6a6
Alexioso <uhopeinsta@gmail.com>
2026-07-29 23:54:02 +0700

    v4.0 tag berbagi tautan, header keamanan, halaman legal

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* HOSANA YOUTH TOOLS - service worker
    PENTING: naikkan angka versi CACHE setiap deploy index.html baru
    supaya cache lama dibuang dan file terbaru dipakai. */
-const CACHE = "pnw-tools-v35";
-const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png"];
+const CACHE = "pnw-tools-v36";
+const APP_SHELL = ["./", "./index.html", "./privacy.html", "./terms.html", "./manifest.json", "./icon-192.png"];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
