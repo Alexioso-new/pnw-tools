ba12d1ff9b1a7279e4712bc836e032e8dee9be81
Alexioso <uhopeinsta@gmail.com>
2026-07-31 01:25:03 +0700

    v4.3: palet baru + ikon Lordicon + atribusi

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* HOSANA YOUTH TOOLS - service worker
    PENTING: naikkan angka versi CACHE setiap deploy index.html baru
    supaya cache lama dibuang dan file terbaru dipakai. */
-const CACHE = "pnw-tools-v42";
+const CACHE = "pnw-tools-v43";
 const APP_SHELL = ["./", "./index.html", "./privacy.html", "./terms.html", "./manifest.json", "./icon-192.png", "./lottie.min.js"];
 
 self.addEventListener("install", (event) => {
